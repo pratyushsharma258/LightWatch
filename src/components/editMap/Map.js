7ea811513c806dd3d42d13702e5c2987859faf57
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2024-01-14 21:38:24 +0530

    Started adding marking feature for new streetlights

---
+++ src/components/editMap/Map.js
@@ -0,0 +1,24 @@
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-defaulticon-compatibility';
+
+export default function Map(props) {
+    const { position, center, zoom } = props
+
+    return (
+        <div className="flex">
+            <MapContainer center={center} zoom={zoom} scrollWheelZoom={false} className={props.className}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+}
--- src/pages/admin/[userId]/index.js
+++ src/pages/admin/[userId]/index.js
@@ -8,6 +8,7 @@ import Deleteicon from "@/components/icons/Deleteicon";
 import Editicon from "@/components/icons/Editicon";
 
 function userpage({ username }) {
+    const buttonStyles = "m-1 text-lg bg-orange-peel text-deepblue shadow-orange-peel hover:text-orange-peel hover:shadow-deepblue shadow-md h-16 rounded-2xl gap-2";
 
     return (
         <div className="w-screen min-h-screen flex flex-col items-center overflow-y-auto relative">
@@ -15,13 +16,12 @@ function userpage({ username }) {
             <Map position={[75.4123, 42.526]} zoom={4} className="max-w-screen max-h-screen absolute left-0 right-0 bottom-0 top-10 z-10" center={[75.4123, 42.526]} />
             <div className="z-20 absolute w-full bg-thistle-blue p-6 rounded-lg h-[13rem] bottom-0 grid grid-cols-2" style={{ boxShadow: "0px -4px 100px 8px black" }}>
                 <Button className="m-1 text-lg bg-orange-peel text-deepblue shadow-orange-peel hover:text-orange-peel hover:shadow-deepblue shadow-md h-16 rounded-2xl gap-2"><Createicon />Add street light</Button>
-                <Button className="m-1 text-lg bg-orange-peel hover:shadow-deepblue text-deepblue shadow-orange-peel hover:text-orange-peel shadow-md h-16 rounded-2xl gap-2"><Usericon />Grievance management</Button>
-                <Button className="m-1 text-lg bg-orange-peel hover:shadow-deepblue text-deepblue shadow-orange-peel hover:text-orange-peel shadow-md h-16 rounded-2xl gap-2"><Editicon />Editing street lights</Button>
-                <Button className="m-1 text-lg bg-orange-peel hover:shadow-deepblue text-deepblue shadow-orange-peel hover:text-orange-peel shadow-md h-16 rounded-2xl gap-2"><Deleteicon />Removing street lights</Button>
+                <Button className={buttonStyles}><Usericon />Grievance management</Button>
+                <Button className={buttonStyles}><Editicon />Editing street lights</Button>
+                <Button className={buttonStyles}><Deleteicon />Removing street lights</Button>
             </div>
         </div>
     );
-
 }
 
 export async function getServerSideProps(context) {
