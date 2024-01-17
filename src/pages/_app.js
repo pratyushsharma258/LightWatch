7ded6b225373de86e8493af1e4c6fa29a31abb29
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2024-01-17 19:55:30 +0530

    Updated font family

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Mulish } from 'next/font/google'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const mulish = Mulish({ subsets: ['latin'] })
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={mulish.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
