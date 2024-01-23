ae3f907d7dc361eecca5d0638cbccb818d2df900
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2024-01-24 01:36:34 +0530

    Completed super admin tasks

--- src/pages/super/[id].js
+++ src/pages/super/[id].js
@@ -1,8 +1,32 @@
 import axios from "axios";
 import Navbar from "@/components/Navbar";
 import AdminDataTable from "@/components/AdminDataTable";
+import { useEffect } from "react";
+import jwt from "jsonwebtoken";
+import { useRouter } from "next/router";
 
 function page({ admins }) {
+  useEffect(() => {
+    const cookies = document.cookie;
+
+    const parsedCookies = cookies.split(";").reduce((acc, cookie) => {
+      const [key, value] = cookie.trim().split("=");
+      acc[key] = value;
+      return acc;
+    }, {});
+
+    jwt.verify(
+      parsedCookies.token,
+      process.env.NEXT_PUBLIC_JWT_SECRET,
+      {},
+      (err, data) => {
+        if (err) console.log("Not found");
+        if (data.userRole !== "super") router.replace("/login");
+      }
+    );
+  }, []);
+
+  const router = useRouter();
   return (
     <div className="min-w-screen min-h-screen bg-deepblue">
       <Navbar />
