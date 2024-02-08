92ada9114f997dd92bfde5d536a67c9d1cd0aea0
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2024-02-09 01:47:41 +0530

    Fixed deployment errors

--- src/pages/admin/[userId]/index.js
+++ src/pages/admin/[userId]/index.js
@@ -4,13 +4,8 @@ import MarkerMap from "@/components/Marker";
 import { useState, useEffect } from "react";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
-import { useToast } from "@/components/ui/use-toast";
-import { Skeleton } from "@/components/ui/skeleton";
-import { PulseLoader } from "react-spinners";
 
 function Userpage({ content, existingLightInfo, existingGrievanceInfo }) {
-  const { toast } = useToast();
-
   const [userIsMarking, setUserIsMarking] = useState(false);
   const [markerPosition, setMarkerPosition] = useState(
     [
@@ -18,29 +13,12 @@ function Userpage({ content, existingLightInfo, existingGrievanceInfo }) {
       existingLightInfo?.responseObject[1]?.coordinates[1] + 0.0002,
     ] || [0, 0]
   );
-  const [dataContent, setDataContent] = useState(content);
-  const [showToast, setShowToast] = useState(false);
-  const [isCLient, setIsCLient] = useState(false);
 
-  useEffect(() => {
-    if (dataContent === false) {
-      setShowToast(true);
-    }
-  }, [dataContent]);
+  const [isCLient, setIsCLient] = useState(false);
 
   useEffect(() => {
     setIsCLient(true);
   }, []);
-
-  useEffect(() => {
-    if (showToast) {
-      toast({
-        variant: "destructive",
-        title: "Something unexpected occurred",
-        description: "Unable to fetch data from the server",
-      });
-    }
-  }, [showToast]);
 
   return (
     <div className="w-screen min-h-screen flex flex-col">
@@ -50,7 +28,7 @@ function Userpage({ content, existingLightInfo, existingGrievanceInfo }) {
         markingHandler={setUserIsMarking}
         markerPosition={markerPosition}
       />
-      {dataContent && isCLient ? (
+      {isCLient ? (
         <>
           {userIsMarking ? (
             <MarkerMap
@@ -73,11 +51,7 @@ function Userpage({ content, existingLightInfo, existingGrievanceInfo }) {
           )}
         </>
       ) : (
-        <>
-          <Skeleton className="min-w-[67vw] flex items-center justify-center max-h-screen rounded-none absolute right-0 z-10 top-0 left-auto bottom-0 bg-lightblue-400 dark:bg-green-800">
-            <PulseLoader color="#000" size={15} />
-          </Skeleton>
-        </>
+        <></>
       )}
       <Toaster />
     </div>
